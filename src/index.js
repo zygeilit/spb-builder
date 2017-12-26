dfe6c1701b306b158efc86e0d7faf7d3e1059f36
zhangyue <zhangyue@beisen.com>
2017-12-26 21:48:30 +0800

    tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 var fs = require('fs')
 var path = require('path')
+
 var componentsProcessor = require('./components/index')
 var actionsProcessor = require('./actions/index')
+// var testsProcessor = require('./tests/index')
 
 var root = path.join(__dirname, '../')
 var builderConfig = JSON.parse(fs.readFileSync(`${root}.builder.json`, 'utf8'))
@@ -9,9 +11,9 @@ var builderConfig = JSON.parse(fs.readFileSync(`${root}.builder.json`, 'utf8'))
 var initSetting = {
   'rootPath': './demo',
   'componentsFolder': 'components',
-  'actionsFolder': 'actions'
+  'actionsFolder': 'actions',
+  'testsFolder': 'tests'
 }
 
 componentsProcessor.processor(builderConfig.components, initSetting)
-
 actionsProcessor.processor(builderConfig.actions, initSetting)
---
+++ src/tests/index.js
@@ -0,0 +1,37 @@
+let fs = require('fs')
+let path = require('path')
+let templateFormatter = require('mustache')
+
+// 获取组件的基础模版，用于输出生成文件
+let testStringTemplate = fs.readFileSync(path.join(__dirname, 'template.js'), 'utf8')
+
+// 根目录
+let testsFolderPath = ''
+
+exports.processor = function (testsConfig, initSetting) {
+  testsFolderPath = `${initSetting.rootPath}/${initSetting.testsFolder}`
+
+  // 创建组件目录
+  if (!fs.existsSync(testsFolderPath)) {
+    fs.mkdirSync(testsFolderPath)
+  }
+
+  // 循环配置，创建对应的js文件
+  testsConfig.forEach(test => {
+    let {  } = test
+
+    let testFilePath = `${testsFolderPath}/${type}.js`
+
+    // 写组件文件
+    fs.writeFile(
+      testFilePath,
+      templateFormatter.render(testStringTemplate, test),
+      function (err) {
+        if (err) {
+          return console.log(err)
+        }
+        console.log(`The file '${type}' was saved!`)
+      }
+    )
+  })
+}
