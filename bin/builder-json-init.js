853569d81ffff74b0ef14340d3d0b9608c756d09
zhangyue <zhangyue@beisen.com>
2017-12-29 07:48:46 +0800

    code formatter

--- bin/builder-json-init.js
+++ bin/builder-json-init.js
@@ -7,14 +7,19 @@ var program = require('commander')
 
 program
   .arguments('<file>')
+
   .option('-u, --username <username>', 'The user to authenticate as')
+
   .option('-p, --password <password>', 'The user\'s password')
+
   .action(function(file) {
-   co(function * () {
-     var username = yield prompt('username: ');
-     var password = yield prompt.password('password: ');
-     console.log(
-       chalk.red('user: %s pass: %s file: %s', username, password, file)
-     )
-   })
+
+    co(function * () {
+      var username = yield prompt('username: ')
+      var password = yield prompt.password('password: ')
+
+      console.log(
+        chalk.red('user: ' + username + ' pass: ' + password + ' file: ' + file)
+      )
+    })
   })
