0ab2dbdae21f175f68394ccf6f3a426de279450d
zhangyue <zhangyue@beisen.com>
2017-12-29 07:47:07 +0800

    shell bin commands

---
+++ bin/builder-json-init.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node --harmony
+
+var chalk = require('chalk')
+var co = require('co')
+var prompt = require('co-prompt')
+var program = require('commander')
+
+program
+  .arguments('<file>')
+  .option('-u, --username <username>', 'The user to authenticate as')
+  .option('-p, --password <password>', 'The user\'s password')
+  .action(function(file) {
+   co(function * () {
+     var username = yield prompt('username: ');
+     var password = yield prompt.password('password: ');
+     console.log(
+       chalk.red('user: %s pass: %s file: %s', username, password, file)
+     )
+   })
+  })
