ce62bac9e8eb57bd194c4e8e575fa4ece4d7be97
张跃 <zygeilit@icloud.com>
2018-01-04 15:18:34 +0800

    Update .builder.js

--- .builder.js
+++ .builder.js
@@ -1,9 +1,5 @@
 
 {
-  "title": "两列布局组件",
-
-  "description": ["两列布局组件，提供了两个站位嘈", "左侧是主内容显示区域，右侧是相关信息区域"],
-
   "type": "main-right-layout",
 
   "themes": [{
@@ -12,6 +8,26 @@
     "type": "noBorder",
     "description": ["组件可切换的主题，在Tab组件内部可使用"]
   }],
+  
+  /**
+  * 配置文档类型，目前提供两种类型的文档策略
+  * ---
+  * type:
+  *   component: 组件类型文档，会默认提取文档内容更新到组件文档集合中
+  *   tutorial: 类似于公司技术入门介绍引导，让新人快了解公司内部技术栈的教程
+  * ---
+  * main: 模块内的文档从那个目录中提取
+  * ---
+  * document-type: 文档类型，标示编写文档用的语言或扩展名
+  */
+  "docs": {
+    "type": "component", // "tutorial"
+    "title": "两列布局组件",
+    "description": ["两列布局组件，提供了两个站位嘈", "左侧是主内容显示区域，右侧是相关信息区域"],
+
+    "main": "/src/docs",
+    "document-type": "markdown" // jsdoc, txt
+  },
 
   /**
   * 注册当前模块依赖的其他标准模块(带有.builder.js的项目)
