f3df3e8465c683204652ca615be425c1e25d5323
zhangyue <zhangyue@beisen.com>
2018-01-05 10:45:22 +0800

    demo rename

--- .builder.js
+++ .builder.js
@@ -16,6 +16,7 @@
   */
   "docs": {
     "type": "component", // "tutorial"
+
     "title": "两列布局组件",
     "description": ["两列布局组件，提供了两个站位嘈", "左侧是主内容显示区域，右侧是相关信息区域"],
 
@@ -26,8 +27,8 @@
     */
     "paths": {
       "usage": "/src/docs.md",
-      "dome": "/src/dome.js"
-    }
+      "demo": "/src/demo.js"
+    },
 
     // 文档类型，用于server端处理
     "document-type": "markdown" // jsdoc, txt
