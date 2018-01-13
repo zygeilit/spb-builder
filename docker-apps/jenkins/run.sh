80bd180c059dd39fc60ccbfddd3302a18a4271ef
zhangyue <zhangyue@beisen.com>
2018-01-13 17:44:11 +0800

    gitlab config

--- docker-apps/jenkins/run.sh
+++
@@ -1,2 +0,0 @@
-docker build jenkins -t "jenkins-service-image"
-docker run -it -p 5001:8080 --volume ..?/Dockerfiles/jenkins/resources:/opt/volume-resources --name jenkins-service jenkins-service-image
