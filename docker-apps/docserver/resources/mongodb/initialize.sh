63d4c6029f413e97406d70502c81082517166f5a
zhangyue <zhangyue@beisen.com>
2018-01-14 18:26:47 +0800

    split project

--- docker-apps/docserver/resources/mongodb/initialize.sh
+++
@@ -1,9 +0,0 @@
-use docs
-
-db.docs.insertOne({
-  "name": "test01",
-  "type": "component",
-  "document-type": "markdown",
-  "title": "两列布局组件",
-  "description": ["两列布局组件，提供了两个站位嘈", "左侧是主内容显示区域，右侧是相关信息区域"]
-})
